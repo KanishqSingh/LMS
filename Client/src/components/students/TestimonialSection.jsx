baae863f48df799b3f382bdd1d42533303200072
Kanishq SIngh <singhkanishq06@gmail.com>
2025-02-15 13:04:37 +0530

    Third

--- Client/src/components/students/TestimonialSection.jsx
+++ Client/src/components/students/TestimonialSection.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { dummyTestimonial } from '../../assets/assets';
+import { assets } from '../../assets/assets';
+import { AppContext } from '../../context/AppContext';
+
+const {calculateRating} = useContext(AppContext) 
 
 const TestimonialSection = () => {
   return (
-    <div>
-      <h2>Testimonials</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicptatem, <br/> quas animi corporis fugit quasi cupiditate. Quia!</p>
-    </div>
-  )
-}
+    <div className='pb-14 px-8 md:px-0'>
+      <h2 className='text-3xl font-medium text-gray-800'>Testimonials</h2>
+      <p className='md:text-base text-gray-500 mt-3'>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas animi corporis fugit quasi cupiditate. Quia!
+      </p>
 
-export default TestimonialSection
+      <div className='mt-8 grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
+        {dummyTestimonial.map((testimonial, index) => (
+          <div key={index} className='border p-5 rounded-lg shadow-md'>
+            <div className='flex items-center gap-4'>
+              <img src={testimonial.image} alt={testimonial.name} className='w-12 h-12 rounded-full' />
+              <div>
+                <h3 className='text-lg font-semibold text-gray-800'>{testimonial.name}</h3>
+                <p className='text-gray-600'>{testimonial.role}</p>
+              </div>
+            </div>
+
+            <div className='mt-4'>
+              <div className='flex gap-0.5'>
+                {[...Array(5)].map((_, i) => (
+                  <img
+                    key={i}
+                    src={i < calculateRating(testimonial.rating) ? assets.star : assets.star_blank}
+                    alt=''
+                    className='w-4 h-4'
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialSection;
